9663c170e645fe4b0e39c34f696963d92c21cf85
dylan <dylanfruit@outlook.fr>
2022-06-13 15:42:24 +0200

    Début du projet

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Error404 from "./pages/Error404";
+import Home from "./pages/Home";
+import Lodging from "./pages/Lodging";
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
